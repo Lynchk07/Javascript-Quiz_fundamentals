22b57dcd726f29a43ff558cf5344de6f09e1b6f0
Lynchk07 <lynch.kristinj@gmail.com>
2022-07-30 09:55:19 -0700

    push 9:55am

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
-//count down timer - Right hand side in CSS
-const startmin = 10;
-//changes the time 
-let time = startmin * 60;
+//highs score hyperlink that pops into a different web browser
+        //highscore page
+//When I click the start button - start button and information are on one page
+// button a timer starts
+    //style:timer in the right hand corner
+//When you answer a question 
+    //There should be 5 questions
+//presented with another question
+//when you answer a question incorrectly time is substracted from the clock 
+//When all questions are answered the timer =0
+//Then the game is over
+//when the game is over you can use local storage to save initials and score.
+//
 
-const countdownEl = document.getElementById('countdown');
 
+//Questions
 
+//Inside which HTML element do we put the Javascript?
+//<scripting>
+//<timisrad>
+//<js>
+//<script>
 
-function countdownUpdate () {
-    const minutes = Math.floor(time /60);
+//Which of the following keywords is used to define a variable in Javascript?
+//var
+//tar
+//x
+//math
 
-    let seconds = time % 60;
+//Which of the following methods can be used to display data in some form of using javascript?
+// document.write()
+//console.log()
+//window.alert()
+//All the above
 
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+//Which of the following methods is used to access HTML elements using javascript?
+//getElementbyid()
+//getElementByTomorrow()
+//getItStarted()
+//getIT()
 
-    countdownEl.innerHTML =  `${seconds}`;
-    time--;
-}
-
-setInterval(countdownUpdate,1000);
-
-//quiz prompts that change the webpage when answered
-
-// recorded score
-
-//Text box entry form that logs score and name
-
-//Ability to clear data from the text box form
+//The condition in an if/else statement is enclosed with ?
+//quotes
+//curly brackets
+// parenthesis
+// square brackets
