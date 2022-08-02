d1b9893c66ddd28997f5399789ef41f584499357
Lynchk07 <lynch.kristinj@gmail.com>
2022-08-01 18:04:30 -0700

    updated javascript,css,html

--- assets/script.js
+++ assets/script.js
@@ -1,71 +1,112 @@
 // variables for starting page 
-var mainEl = document.querySelector(".title")
-var startButton = document.querySelector(".start")
-var que = 0
-var quiz = document.querySelector(".question_text")
+var mainEl = document.querySelector(".container")
+const startButton = document.getElementById('start-b')
+const nextButton = document.getElementById('next-b')
+const questContainerEl = document.getElementById('questions-container')
+const questionEl = document.getElementById('quiz-question')
+const optionButton = document.getElementById('answerbuttons')
+var qi = 0
+
 
 //Function for the start button - start button and information are on one page
-startButton.addEventListener("click",function(){
-    mainEl.setAttribute("class","start_hide")
-    quiz.removeAttribute("class","start_hide")
-    quizQuestions()
+startButton.addEventListener("click", function () {
+    console.log('Start')
+    startButton.classList.add('hide')
+    questionIndex = [0]
+    questContainerEl.classList.remove('hide')
+    startNextQuestionTIMER()
+    startNextQuestion()
+    highscore()
 })
 
 // click start button a timer starts
-    //style:timer in the right hand corner
-    var count=20;
-    var interval=setInterval(function(){
+//style:timer in the right hand corner
+
+function startNextQuestionTIMER() {
+    var count = 20;
+    var interval = setInterval(function () {
         document.getElementById('count').innerHTML = count;
-       count--;
-        if(count === 0){
+        count--;
+        if (count === 0) {
             clearInterval(interval);
             document.getElementById('count').innerHTML = "Completed!";
             alert("Out of time!");
-         }
-}, 1000);    
+        }
+    }, 1000);
+}
+//Quiz Questions and options and answers
+var questions = [
 
-//Quiz Questions and options
-var questions=[
+    {
+        text: "Commonly used data types DO not include.",
+        options:
+            ["a.booleans", "b. Strings", "c. Alerts", "d. Numbers"],
+        answer: "a"
+    },
 
-    {text:"Commonly used data types DO not include.",
-    options:
-    ["a.booleans","b. Strings", "c. Alerts","d. Numbers"],
-    answer:"a"},
+    {
+        text: "Which of the following methods is used to access HTML elements using javascript?",
+        options: ["a. getElementbyid()", "b. getElementByTomorrow()", "c. getItStarted()", "d. getIT()"],
+        answer: "a"
+    },
 
-    {text:"Which of the following methods is used to access HTML elements using javascript?",
-    options:["a. getElementbyid()","b. getElementByTomorrow()", "c. getItStarted()","d. getIT()"],
-    answer:"a"},
+    {
+        text: "The condition in an if/else statement is enclosed with?",
+        options:
+            ["a. quotes", "b. curly brackets", "c. parenthesis", "d. square brackets"],
+        answer: "b"
+    },
 
-    {text:"The condition in an if/else statement is enclosed with?",
-    options:
-    ["a. quotes","b. curly brackets", "c. parenthesis","d. square brackets"],
-    answer:"b"},
-
-    {text:"Which of the following keywords is used to define a variable in Javascript?",
-    options:
-    ["a. var","b. let", "c. const","d. all the above"],
-    answer:"d"},
+    {
+        text: "Which of the following keywords is used to define a variable in Javascript?",
+        options:
+            ["a. var", "b. let", "c. const", "d. all the above"],
+        answer: "d"
+    },
 ]
 
-//First Question 
-function quizQuestions(){
-    document.querySelector(".question_text").textContent = questions[que].text
-    document.querySelector(".answers").innerHTML=""
-    questions[que].options.forEach(function(options)
-    {
+//Question Functions 
+function startNextQuestion() {
+    document.querySelector("#quiz-question").textContent = questions[qi].text
+    document.querySelector("#answerbuttons").innerHTML = ""
+    questions[qi].options.forEach(function (option, i) {
+        // console.log(i)
+        // console.log(option)
+
         var btn = document.createElement("button")
-        btn.textContent = options
-        btn.setAttribute("value",options)
-        btn.onclick = evaluateAnswer
-        document.querySelector(".answers").appendChild(btn)
+        btn.textContent = option;
+        btn.setAttribute("value", option)
+        btn.onclick = evaluateAnswer;
+        document.querySelector("#answerbuttons").appendChild(btn)
     })
 }
 
-function evaluateAnswer(){
-    console.log(this.value)
-    que++;
-    quizQuestions()
+function evaluateAnswer() {
+    console.log(questions[qi].answer)
+    console.log(this.value);
+    console.log(questions[qi].answer == this.value[0])
+
+    if (this.value[0] === questions[qi].answer) {
+        console.log("correct")
+    } else console.log ('incorrect')
+
+    qi++;
+    startNextQuestion();
 }
 
 //next Quiz Div populates 
-var quiz=document.querySelector(".question_text")
+var quiz = document.querySelector(".quiz-question")
+
+//highscore function
+function highscore() {
+    var score = 0;
+    var interval = setInterval(function () {
+        document.getElementById('score').innerHTML = count;
+        count++;
+        if (count === 100) {
+            clearInterval(interval);
+            document.getElementById('score').innerHTML = "Completed!";
+            alert("YOU WON!");
+        }
+    }, 1000);
+}
